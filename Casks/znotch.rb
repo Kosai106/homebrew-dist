373b2836837eb3f4e71450459cad2c1bad707d8a
zkondor <znotch@kondor.xyz>
2023-01-29 03:18:13 +0100

    Initial commit

---
+++ Casks/znotch.rb
@@ -0,0 +1,12 @@
+cask "znotch" do
+  version "1.0.0"
+  sha256 "2392edc098f3aa41a3d467005679792a9856da4cb02c4c194d074ffe39fc17d0"
+
+  url "https://github.com/zkondor/znotch/releases/download/v1.0.0/znotch-1.0.0.dmg"
+  name "zNotch"
+  desc "A tool to toggle Macbook's infamous notch in one click with automation support"
+  homepage "https://github.com/zkondor/znotch"
+
+  app "zNotch.app"
+end
+
